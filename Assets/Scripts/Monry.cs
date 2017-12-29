777f05604055ccb1ecd7d055d582b20ab0ca52b3
Tetsuya Mori <monry84@gmail.com>
2017-12-29 18:12:12 +0900

    :new: Add scripts
    * Spawner と Monry スクリプト

---
+++ Assets/Scripts/Monry.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Schoo01 {
+
+    public class Monry : MonoBehaviour {
+
+        private void Start() {
+            this.GetComponent<RectTransform>().anchoredPosition = new Vector2(
+                Random.Range(-512.0f, 512.0f),
+                Random.Range(-384.0f, 384.0f)
+            );
+            this.StartCoroutine(this.DestroyAfterFewSeconds());
+        }
+
+        private IEnumerator DestroyAfterFewSeconds() {
+            yield return new WaitForSeconds(Random.Range(5.0f, 10.0f));
+            Destroy(this.gameObject);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Schoo01 {
+
+    public class Spawner : MonoBehaviour {
+
+        public GameObject PrefabMonry;
+
+        private IEnumerator Start() {
+            while (true) {
+                Instantiate(this.PrefabMonry, this.transform);
+                yield return new WaitForSeconds(Random.Range(0.5f, 2.0f));
+            }
+        }
+
+    }
+
+}
